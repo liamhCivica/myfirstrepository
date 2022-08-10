8ef0bd1f76726b3ce5e09e646aef5ef0ac995407
liamhcivica <liam.hill@civica.co.uk>
2022-08-10 14:32:53 +0100

    add logging to see what commands do

--- build.sh
+++ build.sh
@@ -3,8 +3,12 @@
 echo 'building'
 
 touch newfile.txt
+ls
 rm newfile.txt
+ls
 mkdir newdir
+ls
 mv test.exe newdir
+tree
 
 echo 'end of build'
--- test.sh
+++ test.sh
@@ -5,5 +5,6 @@ echo 'testing'
 pwd
 whoami
 echo 'new text' >> test.txt
+cat test.txt
 
 echo 'end of test'
